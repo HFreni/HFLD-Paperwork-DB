be6ee0c35ea08fbe113e168dbabbce8dd7d35762
Harrison Freni <hfreni@harrisonfreni.me>
2018-04-16 16:22:34 -0700

    Nested Attributes/

--- app/models/color.rb
+++ app/models/color.rb
@@ -1,4 +1,4 @@
 class Color < ApplicationRecord
     has_many :color_frames
-    has_many :spotlights, through: :color_frames
+    has_many :spotlights, through: :color_frame
 end
--- app/models/color_frame.rb
+++ app/models/color_frame.rb
@@ -1,4 +1,9 @@
 class ColorFrame < ApplicationRecord
   belongs_to :color
   belongs_to :spotlight
+
+  validates_presence_of :color
+  validates_presence_of :spotlight
+
+  accepts_nested_attributes_for :color
 end
