894a002d3fabdf14200878f28ea0c9daa906c716
Brendan Tendrich <brendan@tendrich.net>
2018-04-17 12:45:02 -0400

    added contrasting text color to gel name on top of swatch color

--- app/models/color.rb
+++ app/models/color.rb
@@ -7,4 +7,13 @@ class Color < ApplicationRecord
     def full_name
       "#{gel_num} - #{gel_name}"
     end
+    
+    def luma
+      rgb = gel_hex.gsub(/^\#/,'').scan(/../).map{|x| x.hex}
+      r = rgb[0]
+      g = rgb[1]
+      b = rgb[2]
+
+      Math.sqrt(0.299 * r**2 + 0.587 * g**2 + 0.114 * b**2).round
+    end
 end
