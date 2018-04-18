4986fccebba249851a156caaa67802fed2526f12
Harrison Freni <hfreni@harrisonfreni.me>
2018-04-17 17:33:01 -0700

    Model stuff!

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,3 +1,3 @@
 class Person < ApplicationRecord
-    belongs_to :spotlight
+
 end
--- app/models/spotlight.rb
+++ app/models/spotlight.rb
@@ -2,9 +2,9 @@ class Spotlight < ApplicationRecord
     has_many :color_frames, dependent: :destroy, inverse_of: :spotlight
     has_many :colors, through: :color_frames
     
+    belongs_to :spotlight_op, :class_name => "Person"
     has_many :spotlight_positions
     has_many :spotlight_models
-    has_many :people
     
     accepts_nested_attributes_for :color_frames, allow_destroy: true
 end
