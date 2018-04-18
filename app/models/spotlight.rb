b847554460373bc8aff2222a75a88f54cbe7f274
Harrison Freni <hfreni@harrisonfreni.me>
2018-04-17 17:08:00 -0700

    Still Defining Models

--- app/models/spotlight.rb
+++ app/models/spotlight.rb
@@ -1,6 +1,7 @@
 class Spotlight < ApplicationRecord
     has_many :color_frames, dependent: :destroy, inverse_of: :spotlight
     has_many :colors, through: :color_frames
+    
     has_many :spotlight_positions
     has_many :spotlight_models
     has_many :people
