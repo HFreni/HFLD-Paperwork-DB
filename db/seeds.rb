6cb21720fc4fd815231c7d6fb3d3b01cf0849440
Brendan Tendrich <brendan@tendrich.net>
2018-04-28 10:42:44 -0400

    made scaffold views more pretty

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,24 +21,28 @@ Person.create({:id => 1, :person_fname => 'Deon', :person_lname => 'Italiano' })
 Person.create({:id => 2, :person_fname => 'Neville', :person_lname => 'Ebner' })
 Person.create({:id => 3, :person_fname => 'Lorenzo', :person_lname => 'Haskin' })
 Person.create({:id => 4, :person_fname => 'Fermin', :person_lname => 'Rusek' })
+ActiveRecord::Base.connection.execute("ALTER SEQUENCE public.people_id_seq RESTART WITH 1000")
 
 # Build a couple generic spotlight models
 SpotlightModel.create({id: 1, spotlight_weight: rand(100)+100, manufacturer_id: 1, spotlight_name: 'Generic Spotlight Type 1'})
 SpotlightModel.create({id: 2, spotlight_weight: rand(100)+100, manufacturer_id: 1, spotlight_name: 'Generic Spotlight Type 2'})
 SpotlightModel.create({id: 3, spotlight_weight: rand(100)+100, manufacturer_id: 1, spotlight_name: 'Generic Spotlight Type 3'})
 SpotlightModel.create({id: 4, spotlight_weight: rand(100)+100, manufacturer_id: 1, spotlight_name: 'Generic Spotlight Type 4'})
+ActiveRecord::Base.connection.execute("ALTER SEQUENCE public.spotlight_models_id_seq RESTART WITH 1000")
 
 # Build a couple generic spotlight positions
 SpotlightPosition.create({id: 1, position_name: "House Left"})
 SpotlightPosition.create({id: 2, position_name: "House Right"})
 SpotlightPosition.create({id: 3, position_name: "Projection Booth"})
 SpotlightPosition.create({id: 4, position_name: "Upstage Truss"})
+ActiveRecord::Base.connection.execute("ALTER SEQUENCE public.spotlight_positions_id_seq RESTART WITH 1000")
 
 # Build a couple generic spotlights
 Spotlight.create({id: 1, spotlight_name: 'Spot 1', operator_id: 1, model_id: 1, position_id: 1 })
 Spotlight.create({id: 2, spotlight_name: 'Spot 2', operator_id: 2, model_id: 1, position_id: 3 })
 Spotlight.create({id: 3, spotlight_name: 'Spot 3', operator_id: 3, model_id: 1, position_id: 2 })
 Spotlight.create({id: 4, spotlight_name: 'Rear Spot', operator_id: 4, model_id: 2, position_id: 4 })
+ActiveRecord::Base.connection.execute("ALTER SEQUENCE public.spotlights_id_seq RESTART WITH 1000")
 
 # Populate some spotlights with some colors
 color_frame_index = 1
@@ -51,12 +55,14 @@ Spotlight.all.each do |spotlight|
   ColorFrame.create({id: color_frame_index+5, color_id: 261, spotlight_id: spotlight.id, frame_position: 6 })
   color_frame_index = color_frame_index + 6
 end
+ActiveRecord::Base.connection.execute("ALTER SEQUENCE public.color_frames_id_seq RESTART WITH 1000")
 
 # Populate some SpotCue's
 SpotCue.create( {id: 1, number: 141.0, light_cue_number: 141.0, cue_type: "Scene", name: "ACT TWO", notes: ""})
 SpotCue.create( {id: 2, number: 142.0, light_cue_number: 142.0, cue_type: "Song", name: "\"Pierre\"", notes: ""})
 SpotCue.create( {id: 3, number: 142.1, cue_type: "Regular", name: "With Cue 142", notes: ""})
 SpotCue.create( {id: 4, number: 142.2, cue_type: "Regular", name: "Actor 2 heads downstage", notes: ""})
+ActiveRecord::Base.connection.execute("ALTER SEQUENCE public.spot_cues_id_seq RESTART WITH 1000")
 
 # Populate some SpotCuesSpotlight's
 SpotCuesSpotlight.create({id: 1, spot_cue_id: 1, spotlight_id: 1, size: "n/a", intensity: "n/a", action: "Wake up!", time: 3, notes: ""})
@@ -64,3 +70,4 @@ SpotCuesSpotlight.create({id: 2, spot_cue_id: 1, spotlight_id: 2, size: "n/a", i
 SpotCuesSpotlight.create({id: 3, spot_cue_id: 3, spotlight_id: 1, size: "1/2 B", intensity: "75%", action: "PU Actor 1", time: 3, notes: ""})
 SpotCuesSpotlight.create({id: 4, spot_cue_id: 3, spotlight_id: 2, size: "1/8 B", intensity: "100%", action: "PU Actor 2", time: 3, notes: ""})
 SpotCuesSpotlight.create({id: 5, spot_cue_id: 4, spotlight_id: 2, size: "1/8 B", intensity: "100%", action: "Fall Asleep", time: 3, notes: ""})
+ActiveRecord::Base.connection.execute("ALTER SEQUENCE public.spot_cues_spotlights_id_seq RESTART WITH 1000")
