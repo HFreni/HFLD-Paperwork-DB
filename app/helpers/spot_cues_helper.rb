e55997192bfe64939af7933b70924bb2b22f0565
Brendan Tendrich <brendan@tendrich.net>
2018-04-28 20:34:26 -0400

    make matrix view a little more pretty

--- app/helpers/spot_cues_helper.rb
+++ app/helpers/spot_cues_helper.rb
@@ -1,2 +1,13 @@
 module SpotCuesHelper
+
+  def background_color_for_cue_type(cue_type)
+    case cue_type
+    when "Song"
+      "#6495ed"
+    when "Scene"
+      "#ff69b4"
+    else
+      "#ffffff"
+    end
+  end
 end
